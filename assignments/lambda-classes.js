004ea564ee77abe2cc2064b9a22060738ad5a734
Maaruf Dauda <mkdauda.work@gmail.com>
2019-08-22 14:35:58 +0100

    Create class Instructor to extend Person

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,3 +10,20 @@ class Person {
 		console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
 	}
 }
+
+class Instructor extends Person {
+	constructor(name, age, location, specialty, faveLanguage, catchPhrase) {
+		super(name, age, location);
+		this.specialty = specialty;
+		this.faveLanguage = faveLanguage;
+		this.catchPhrase = catchPhrase;
+	}
+
+	demo(subject){
+		console.log(`Today we are learning about ${subject}`)
+	}
+
+	grade(student, subject) {
+		console.log(`${student.name} receives a perfect score on ${subject}`)
+	}
+}
