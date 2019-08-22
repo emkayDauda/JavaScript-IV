b69fa23a789e2cfdab1c97058ec2a52ed1a97579
Maaruf Dauda <mkdauda.work@gmail.com>
2019-08-22 14:42:07 +0100

    Create class Student to extend Person

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -27,3 +27,25 @@ class Instructor extends Person {
 		console.log(`${student.name} receives a perfect score on ${subject}`)
 	}
 }
+
+
+class Student extends Person {
+	constructor(name, age, location, previousBackground, className, favSubjects){
+		super(name, age, location);
+		this.previousBackground = previousBackground;
+		this.className = className;
+		this.favSubjects = favSubjects;
+	}
+
+	listsSubjects(){
+		this.favSubjects.array.forEach(element => console.log(element));
+	}
+
+	PRAssignment(subject) {
+		console.log(`${this.name} has submitted a PR for ${subject}`)
+	}
+
+	sprintChallenge(subject) {
+		console.log(`${this.name} has begun sprint challenge on ${subject}`)
+	}
+}
